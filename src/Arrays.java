b7b1d3ba4b83fb30d0218d72a8995bc5887cef8e
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2022-12-25 04:47:25 +0530

    change array class name

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class arrays {
+public class Arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any 5 numbers");
