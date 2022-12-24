343fc41de897cdf27457c820fb185698655ea954
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2022-12-25 04:45:43 +0530

    checking of power method in java

--- src/String_Problem.java
+++ src/String_Problem.java
@@ -19,11 +19,7 @@ public class String_Problem {
         String letter = "Dear <|name|>, Thanks a lot";
         letter=letter.replace("<|name|>","Shubham");
         System.out.println(letter);
-//
-//
-//
-//
-//
+
 //      4.  Write a Java program to detect double and triple spaces in a string.
          String space = "HEllo  My name is   Shubham";
         System.out.println(space.indexOf("  "));
