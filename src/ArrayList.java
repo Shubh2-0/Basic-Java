85740744ff15151db1793b5a880b69a92ca6e74f
Shubham Bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-01-25 03:53:28 +0530

    Create ArrayList.java

---
+++ src/ArrayList.java
@@ -0,0 +1,23 @@
+import java.util*;
+
+public class Array_List{
+
+
+public static void main(String[] args){
+
+List<Integer> arr = new ArrayList<>();
+  
+  arr.add(1);
+  arr.add(2);
+  arr.add(3);
+  arr.add(4);
+  arr.add(5);
+  
+  System.out.println(arr);
+  
+
+}
+
+
+
+}
